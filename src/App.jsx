37ba547d3c39dfd9072999da56b3bc5f82077ba0
mohabyousry <72094368+mohabyousry@users.noreply.github.com>
2026-02-10 01:55:53 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="center">
+      <h1>Video Transcriber</h1>
+<div class="d">
+      <div className="box">
+        <p>Upload your video here</p>
+        <button>Upload</button>
+      </div>
+
+      <div className="box">
+        <p>
+          view your subtitles here
+        </p>
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
